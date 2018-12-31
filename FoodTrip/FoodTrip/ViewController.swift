3d2742351e24b5e9b7f2280fe3242a03782dadee
Daiho-Kim <46131182+Daiho-Kim@users.noreply.github.com>
2018-12-31 15:53:47 +0900

    Add files via upload
    First Commit

---
+++ FoodTrip/FoodTrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrip
+//
+//  Created by Daiho Kim on 31/12/2018.
+//  Copyright © 2018 Daiho Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
